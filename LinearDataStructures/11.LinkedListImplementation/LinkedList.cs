0ba6833b1f2eb20c96247ea7138d4e697c8629d4
Nikolay Iliev <iliev.nick007@gmail.com>
2015-10-29 16:51:53 +0200

    add simple LinkedList implementation
    add simple LinkedList implementation
    (task 11)

---
+++ LinearDataStructures/11.LinkedListImplementation/LinkedList.cs
@@ -0,0 +1,36 @@
+﻿namespace _11.LinkedListImplementation
+{
+    public class LinkedList<T>
+    {
+        private Node head;
+
+        public void AddNode(T data)
+        {
+            Node newNode = new Node();
+            newNode.Next = this.head;
+            newNode.Data = data;
+            this.head = newNode;
+        }
+
+        public T GetFirstAdded()
+        {
+            T temp = default(T);
+
+            Node current = this.head;
+            while (current != null)
+            {
+                temp = current.Data;
+                current = current.Next;
+            }
+
+            return temp;
+        }
+
+        private class Node
+        {
+            public Node Next { get; set; }
+
+            public T Data { get; set; }
+        }
+    }
+}
---
+++ LinearDataStructures/11.LinkedListImplementation/Startup.cs
@@ -0,0 +1,19 @@
+﻿namespace _11.LinkedListImplementation
+{
+    using System;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            LinkedList<int> sampleList = new LinkedList<int>();
+            sampleList.AddNode(-33);
+            sampleList.AddNode(2);
+            sampleList.AddNode(3);
+            sampleList.AddNode(4);
+            int initialValue = sampleList.GetFirstAdded();
+
+            Console.WriteLine(initialValue);
+        }
+    }
+}
